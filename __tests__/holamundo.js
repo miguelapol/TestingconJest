989af96ca49f03885f9290bd5988946e05838457
miguelapol <apolonio1995@gmail.com>
2022-06-19 11:22:09 -0500

    pruebas de jest con hola mundo

--- __tests__/holamundo.js
+++ __tests__/holamundo.js
@@ -1,1 +1,9 @@
-test('Hola mundo en jest', () => {});
+describe('Grupo de pruebas',()=>{
+    test('Hola mundo en jest', () => {
+    });
+    test('Otro hola mundo', () => {
+    });
+})
+
+
+//se puede usar test o it hacen lo mismo
