2d91def0649bfc57fd1b15f0f2dea08960897f8b
miguelapol <apolonio1995@gmail.com>
2022-06-19 11:55:39 -0500

    comprobando resta y not suma

--- __tests__/ejemplo_funciones.js
+++ __tests__/ejemplo_funciones.js
@@ -9,4 +9,10 @@ describe('Testing functions de suma y resta', () => {
     test('suma de 2 y 3', () => {
         expect(suma(2, 3)).toBe(5);
     })
+    test('resta de 5 y 2', () => {
+        expect(resta(5, 4)).toBe(1);
+    })
+    test('que la suma 10 y 10 nose 10', () => {
+        expect(suma(10, 10)).not.toBe(10);
+    })
 })
