d0ce76ac311fa7090f2578f74168eeb02f456347
miguelapol <apolonio1995@gmail.com>
2022-06-22 10:59:23 -0500

    aqui usamos snapshots que compara objetos

--- __tests__/ejemplosnapshots.js
+++ __tests__/ejemplosnapshots.js
@@ -1,1 +1,14 @@
-const cliente
+const cliente={
+    nombre:'Juan Pablo 2',
+    balance:500,
+    tipo:'Premium'
+}
+//los snapshots se hace al objeto completo se almacenan en un string y comparan si son el mismo
+//dato mismo cliente o diferente
+//usando el npm -- -u actualiza el snapshot
+describe('Testing al cliente',()=>{
+    test('Es Juan Pablo',()=>{
+        expect(cliente).toMatchSnapshot();
+
+    })
+})
